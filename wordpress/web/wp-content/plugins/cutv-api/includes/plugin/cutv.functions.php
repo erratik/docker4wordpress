10504461629908d4c29b5aede0b30c1cefe578a7
erratik <tay@erratik.ca>
2019-06-10 22:38:31 -0400

    [chore/fix/channel] fixed channels, error in response

--- wordpress/web/wp-content/plugins/cutv-api/includes/plugin/cutv.functions.php
+++ wordpress/web/wp-content/plugins/cutv-api/includes/plugin/cutv.functions.php
@@ -2,30 +2,18 @@
 
 
     /* Require CUTV Functions */
-    require_once( CUTV_PATH. 'functions/cutv.functions.admin.php');
-
-	/* Require String Functions */
-	require_once( CUTV_PATH. 'functions/cutv.functions.strings.php');
-
+    require_once( CUTV_PATH. 'helpers/cutv.helpers.snaptube.php');
+    require_once( CUTV_PATH. 'helpers/cutv.helpers.sources.php');
+    require_once( CUTV_PATH. 'helpers/cutv.helpers.videos.php');
+    require_once( CUTV_PATH. 'helpers/cutv.helpers.strings.php');
 
 
 	// todo: check if used once everything works?
 
-	/* Require Source Functions */
 	require_once( CUTV_PATH. 'functions/cutv.functions.globals.php');
-
-	/* Require Source Functions */
-	require_once( CUTV_PATH. 'functions/cutv.functions.sources.php');
-
-	/* Require Videos Functions */
-	require_once( CUTV_PATH. 'functions/cutv.functions.videos.php');
 	
-	/* Require Source Functions */
+	require_once( CUTV_PATH. 'functions/cutv.functions.sources.php');
+	require_once( CUTV_PATH. 'functions/cutv.functions.channels.php');
 	require_once( CUTV_PATH. 'functions/cutv.functions.snaptubes.php');
-
-	/* Require Source Functions */
+	
 	require_once( CUTV_PATH. 'functions/cutv.functions.core.php');
-
-
-	/* Require Source Functions */
-	require_once( CUTV_PATH. 'functions/cutv.functions.utils.php');
